ed20ce3b65cfd923d3705b6b2ba03a1d5d2c0a84
alistair0111 <alistair.baretto@gmail.com>
2021-07-06 01:00:12 +0530

    dashboard calendar updated

---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { withAuthentication } from '../Session';
+import moment from 'moment';
+
+import Grid from '@material-ui/core/Grid';
+
+import CalendarBody from './calendar-body';
+import CalendarHead from './calendar-head';
+
+function Calendar(props) {
+	const { firebase, authUser } = props;
+	let defaultSelectedDay = {
+		day: moment().format('D'),
+		month: moment().month(),
+	};
+
+	/*** HOOKS ***/
+	const [dateObject, setdateObject] = useState(moment());
+	const [showMonthTable, setShowMonthTable] = useState(false);
+	const [selectedDay, setSelected] = useState(defaultSelectedDay);
+
+	const allMonths = moment.months();
+	const currentMonth = () => dateObject.format('MMMM');
+	const currentYear = () => dateObject.format('YYYY');
+
+	const setMonth = (month) => {
+		let monthNo = allMonths.indexOf(month);
+		let newDateObject = Object.assign({}, dateObject);
+		newDateObject = moment(dateObject).set('month', monthNo);
+		setdateObject(newDateObject);
+		setShowMonthTable(false);
+	};
+
+	const toggleMonthSelect = () => setShowMonthTable(!showMonthTable);
+
+	const setSelectedDay = (day) => {
+		setSelected({
+			day,
+			month: currentMonthNum(),
+		});
+		// Later refresh data
+	};
+
+	const currentMonthNum = () => dateObject.month();
+	const daysInMonth = () => dateObject.daysInMonth();
+	const currentDay = () => dateObject.format('D');
+	const actualMonth = () => moment().format('MMMM');
+
+	const firstDayOfMonth = () => moment(dateObject).startOf('month').format('d');
+
+	return (
+		<Grid container spacing={3}>
+			<Grid item xs={12} md={8} lg={9}>
+				<CalendarHead
+					allMonths={allMonths}
+					currentMonth={currentMonth}
+					currentYear={currentYear}
+					setMonth={setMonth}
+					showMonthTable={showMonthTable}
+					toggleMonthSelect={toggleMonthSelect}
+				/>
+				<CalendarBody
+					firstDayOfMonth={firstDayOfMonth}
+					daysInMonth={daysInMonth}
+					currentDay={currentDay}
+					currentMonth={currentMonth}
+					currentMonthNum={currentMonthNum}
+					actualMonth={actualMonth}
+					setSelectedDay={setSelectedDay}
+					selectedDay={selectedDay}
+					weekdays={moment.weekdays()}
+				/>
+			</Grid>
+		</Grid>
+	);
+}
+
+export default withAuthentication(Calendar);
